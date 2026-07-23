4142d9b2b357b74fa6cbae3cfc4f7b79b75641d1
MARIANGEL ROJAS <m0r15vt@cam.wal-mart.com>
2026-07-23 14:56:14 -0600

    mainjava

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Desafío Walmart Tech - Nivel 01");
+        // TODO: Implementar la lógica principal del nivel 1.
+        String[] datos={"A","B","C"};
+        // TODO: Procesar datos relacionados con tiendas.
+    }
+
+    // TODO: Implementar método principal del reto.
+    public static int resolver(String[] datos){
+        // TODO: Agregar algoritmo de complejidad aproximada nivel 1.
+        return 0;
+    }
+}
